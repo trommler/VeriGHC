d05153f8348a4d5fc706dfdc891e6299020f56dc
Peter Trommler <ptrommler@acm.org>
2018-10-11 13:50:27 +0200

    Semantics for CmmType

---
+++ cmm/CmmType_sem.v
@@ -0,0 +1,39 @@
+Require Import GHC.CmmType.
+
+Require Import compcert.lib.Integers.
+Require Import compcert.lib.Floats.
+
+Module Wordsize_16.
+  Definition wordsize := 16%nat.
+  Remark wordsize_not_zero: wordsize <> 0%nat.
+  Proof. unfold wordsize; congruence. Qed.
+End Wordsize_16.
+
+Strategy opaque [Wordsize_16.wordsize].
+
+Module Int16 := Make(Wordsize_16).
+
+Strategy 0 [Wordsize_16.wordsize].
+
+Notation short := Int16.int.
+
+Notation long := Int64.int.
+
+Definition cmmTypeDenote (t:CmmType) :=
+  match t with
+  | CT_CmmType BitsCat w => match w with
+                            | W8 => byte
+                            | W16 => short
+                            | W32 => int
+                            | W64 => long
+                            | _ => unit
+                            end
+  | CT_CmmType FloatCat w => match w with
+                             | W32 => float32
+                             | W64 => float
+                             | _ => unit
+                             end
+  | CT_CmmType GcPtrCat _ => Int64.int
+  | CT_CmmType (VecCat _ _) _ => unit
+  end.
+  
