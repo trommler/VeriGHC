6236502ae3dc8d8578e163019dce5cfce6c447e3
Peter Trommler <ptrommler@acm.org>
2018-10-11 13:51:07 +0200

    Add more MachOps

--- ghc/CmmMachOp.v
+++ ghc/CmmMachOp.v
@@ -1,8 +1,15 @@
 Require Import GHC.CmmType.
 
 Inductive MachOp :=
+  (* Integer operations (insensitive to signed/unsigned) *)
 | MO_Add: Width -> MachOp
-(* ... *)
+| MO_Sub: Width -> MachOp
+| MO_Eq: Width -> MachOp
+| MO_Ne: Width -> MachOp
 | MO_Mul: Width -> MachOp
 (* ... *)
 .
+
+Definition MachOp_eq_dec (m1 m2 : MachOp) : {m1 = m2} + {m1 <> m2}.
+  decide equality; try (apply Width_eq).
+Defined.
