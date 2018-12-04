f9ba0d31c7a633266684ccfb5c104871846528e2
Peter Trommler <ptrommler@acm.org>
2018-12-04 17:08:49 +0100

    Start inductive relation for small-step semantics

--- cmm/CmmNode_sem.v
+++ cmm/CmmNode_sem.v
@@ -1,3 +1,7 @@
+Require Import compcert.lib.Maps.
+Require Import List.
+Import ListNotations.
+
 Require Import compcert.common.Memory.
 Require Import compcert.common.Values.
 
@@ -12,6 +16,47 @@ Require Import CmmType_sem.
 
 Require Import Cminor.
 
+Inductive cont: Type :=
+| Kstop: cont
+| Klist: (list CmmNode) -> cont -> cont
+(*  | Kcall: option ident -> function -> val -> env -> cont -> cont.*)
+.
+
+Definition env := PTree.t val.
+
+Inductive state : Type :=
+| Sequence : forall (* (f:function) *)
+    (n:CmmNode)
+    (k:cont)
+    (sp:val)
+    (e:env)
+    (m:mem),
+    state
+.
+
+Inductive step : state -> state -> Prop :=
+| step_comment : forall n ns k sp e m,
+    step (Sequence CmmComment (Klist (n::ns) k) sp e m)
+         (Sequence n (Klist ns k) sp e m)
+| step_label : forall l n ns k sp e m,
+    step (Sequence (CmmEntry l) (Klist (n::ns) k) sp e m)
+         (Sequence n (Klist ns k) sp e m)
+| step_assign_local : forall reg l t ex v n ns k sp e m,
+    cmmExprDenote m ex = Some v ->
+    cmmExprType ex = t ->
+    CmmLocal (LR_LocalReg l t) = reg ->
+    step (Sequence (CmmAssign reg ex) (Klist (n::ns) k) sp e m)
+         (Sequence n (Klist ns k) sp (PTree.set l v e) m)
+| step_store : forall lexp rexp ptr val ch n ns k sp e m m',
+    cmmExprDenote m lexp = Some ptr ->
+    cmmExprDenote m rexp = Some val ->
+    cmmTypeToChunk (cmmExprType rexp) = ch ->
+    Mem.storev ch m ptr val = Some m' ->
+    step (Sequence (CmmStore lexp rexp) (Klist (n::ns) k) sp e m)
+         (Sequence n (Klist ns k) sp e m') 
+.
+
+(* Monadic 
 
 Inductive answer : Type := 
 | Value : val -> answer 
@@ -28,6 +73,18 @@ Notation "x <- c1 ; c2" := (Bind c1 (fun x => c2))
         (right associativity, at level 84, c1 at next level) : comp_scope.
 Local Open Scope comp_scope.
 
+
+Fixpoint cmmNodeDenote (n:CmmNode) (m:mem) : option mem :=
+  match n with
+  | CmmStore lexp rexpr => match cmmDenote m lexpr, cmmDenote m rexpr, cmmTypeDenote (cmmExprType rexpr) with
+                           | Some ptr, Some val, chunk => Mem.storev chunk m ptr val
+                           | _, _, _ => None
+                           end
+  | CmmComment => Some m
+  | CmmEntry _ => Some m
+  | CmmBranch l =>
+
+*) 
 (*
 Program Fixpoint cmmNodeDenote (node : CmmNode) (m:mem) : comp :=
   match node with
