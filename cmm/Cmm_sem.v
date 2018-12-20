2b42288f71f2f2db367168397eba0b1eb5c89e7e
Peter Trommler <ptrommler@acm.org>
2018-12-20 13:26:34 +0100

    Semantics for entire Cmm programs

--- cmm/Cmm_sem.v
+++ cmm/Cmm_sem.v
@@ -1,26 +1,60 @@
 Require Import Coq.Numbers.BinNums.
+Require Import List.
+Import ListNotations.
 
 Require Import compcert.common.AST.
 Require Import compcert.common.Values.
 Require Import compcert.common.Globalenvs.
 Require Import compcert.common.Memory.
 
+Require Import compcert.lib.Integers.
+Require Import compcert.lib.Floats.
+
+Require Import GHC.CmmType.
 Require Import GHC.Cmm.
+Require Import GHC.Int.
 Require Import Cmm.CmmNode_sem.
+
+Require Import GHC.CmmExpr. (* FIXME: This should be GHC.Rational. See #2 *)
 
 Definition cmm_program := AST.program cmm_fundef unit.
 
 
+Definition cmmIntToinit_data (i:Integer) (w:Width) : init_data :=
+  match w with
+  | W8 =>  Init_int8 (Int.repr i)
+  | W16 => Init_int16 (Int.repr i)
+  | W32 => Init_int32 (Int.repr i)
+  | W64 => Init_int64 (Int64.repr i)
+  | _ =>   Init_int32 Int.zero (* panic *)
+  end.
+
 (* TODO: Work out the details *)
+Definition cmmFloatToInitdata (r:Rational) (w:Width) : init_data :=
+  match w with
+  | W32 => Init_float32 (Float.to_single Float.zero)
+  | W64 => Init_float64 Float.zero
+  | _   => Init_float64 Float.zero
+  end.
+  
+  
 Definition cmmStaticToinit_data (s:CmmStatic) : list init_data :=
   match s with
-  | CmmStaticLit l => nil
-  | CmmUninitialised n => nil
+  | CmmStaticLit l => match l with
+                      | CmmInt i w => [ cmmIntToinit_data i w ]
+                      | CmmFloat r w => [ cmmFloatToInitdata r w ]
+                      | CmmLabel l => [ Init_addrof l (Ptrofs.of_int64 Int64.zero) ]
+                      | CmmLabelOff l off => [ Init_addrof l (Ptrofs.of_int64 off) ]
+                      | CmmLabelDiffOff l1 l2 off w => [ Init_addrof l1 (Ptrofs.of_int64 off) ] (* FIXME: Need pointer difference for this *) 
+                      | CmmBlock b => [ Init_addrof b (Ptrofs.of_int64 Int64.zero) ]
+                      | CmmHighStackMark => [ Init_int64 Int64.zero ]
+                      end
+  | CmmUninitialised n => [Init_space (IntToZ n)]
   | CmmString cs => nil
   end.
 
-Definition cmmStaticsToinit_datas (ss:list CmmStatic) : list init_data :=
-  List.concat (List.map cmmStaticToinit_data ss).
+Definition cmmStaticsToinit_datas (css:list CmmStatic) : list init_data :=
+  List.concat (List.map cmmStaticToinit_data css).
 
 Definition cmmDeclToAST (d:CmmDecl) : (ident * globdef cmm_fundef unit) :=
   match d with
@@ -31,12 +65,10 @@ Definition cmmDeclToAST (d:CmmDecl) : (ident * globdef cmm_fundef unit) :=
 Definition cmmGroupToAST (g:CmmGroup) : list (ident * globdef cmm_fundef unit) :=
   List.map cmmDeclToAST g.
 
-(* TODO: Finish implementation *)
-  
 Definition cmmProgramToAST (cmm_prog:CmmProgram) : cmm_program :=
   {| prog_defs := List.concat (List.map cmmGroupToAST cmm_prog);
      prog_public := nil;
-     prog_main := 1%positive
+     prog_main := 1%positive (* FIXME: Where does a Cmm program start? hs_main? *)
   |}.
 
 Inductive initial_state (p:cmm_program) : state -> Prop :=
@@ -46,3 +78,5 @@ Inductive initial_state (p:cmm_program) : state -> Prop :=
     Genv.find_symbol ge p.(prog_main) = Some b ->
     Genv.find_funct_ptr ge b = Some f ->
     initial_state p (CallState f nil Kstop m0).
+
+(* FIXME: What is the final state? *)
--- ghc/Int.v
+++ ghc/Int.v
@@ -3,3 +3,6 @@ Require Import compcert.lib.Integers.
 
 Definition Int := int64.
 Definition Integer := Z.
+
+Definition IntToZ (i:Int) : Z :=
+  Int64.unsigned i.
