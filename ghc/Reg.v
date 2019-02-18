da942b7cbc0d3832ceafc8bcd0a489ea4a9ca3d6
Peter Trommler <ptrommler@acm.org>
2019-02-18 15:05:13 +0100

    Add register definitions

--- ghc/Reg.v
+++ ghc/Reg.v
@@ -1,4 +1,23 @@
-Inductive Reg : Set :=
-| RegVirtual: nat -> Reg
-| RegReal:    nat -> Reg
+(* compiler/nativeGen/Reg.hs *)
+Require Import GHC.Int.
+Require Import GHC.Unique.
+
+Definition RegNo := Int.
+
+Inductive RealReg :=
+| RealRegSingle: RegNo -> RealReg
+| RealRegPair:   RegNo -> RegNo -> RealReg
+.
+
+Inductive VirtualReg :=
+| VirtualRegI:   Unique -> VirtualReg
+| VirtualRegHi:  Unique -> VirtualReg
+| VirtualRegF:   Unique -> VirtualReg
+| VirtualRegD:   Unique -> VirtualReg
+| VirtualRegSSE: Unique -> VirtualReg
+.
+
+Inductive Reg :=
+| RegVirtual: VirtualReg -> Reg
+| RegReal:    RealReg -> Reg
 .
