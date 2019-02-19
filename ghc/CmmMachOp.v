611acf250df2dbe679d59cc11d3e3e963227e3c8
Peter Trommler <ptrommler@acm.org>
2019-02-19 17:02:55 +0100

    Expand Cmm machine operations

--- ghc/CmmMachOp.v
+++ ghc/CmmMachOp.v
@@ -8,23 +8,165 @@ Inductive MachOp :=
 | MO_Eq: Width -> MachOp
 | MO_Ne: Width -> MachOp
 | MO_Mul: Width -> MachOp
-(* ... *)
+(* Signed multiply/divide *)
+| MO_S_MulMayOflo: Width -> MachOp
+| MO_S_Quot: Width -> MachOp
+| MO_S_Rem: Width -> MachOp
+| MO_S_Neg: Width -> MachOp
+(* Unsigned multiply/divide *)
+| MO_U_MulMayOflo: Width -> MachOp
+| MO_U_Quot: Width -> MachOp
+| MO_U_Rem: Width -> MachOp
+(* Signed comparisons *)
+| MO_S_Ge: Width -> MachOp
+| MO_S_Le: Width -> MachOp
+| MO_S_Gt: Width -> MachOp
+| MO_S_Lt: Width -> MachOp
+(* Unsigned comparisons *)
+| MO_U_Ge: Width -> MachOp
+| MO_U_Le: Width -> MachOp
+| MO_U_Gt: Width -> MachOp
+| MO_U_Lt: Width -> MachOp
+(* Floating point arithmetic *)
+| MO_F_Add: Width -> MachOp
+| MO_F_Sub: Width -> MachOp
+| MO_F_Neg: Width -> MachOp
+| MO_F_Mul: Width -> MachOp
+| MO_F_Quot: Width -> MachOp
+(* Floating point comparison *)
+| MO_F_Eq: Width -> MachOp
+| MO_F_Ne: Width -> MachOp
+| MO_F_Ge: Width -> MachOp
+| MO_F_Le: Width -> MachOp
+| MO_F_Gt: Width -> MachOp
+| MO_F_Lt: Width -> MachOp
+(* Bitwise operations.  Not all of these may be supported
+   at all sizes, and only integral Widths are valid. *)
+| MO_And: Width -> MachOp
+| MO_Or: Width -> MachOp
+| MO_Xor: Width -> MachOp
+| MO_Not: Width -> MachOp
+| MO_Shl: Width -> MachOp
+| MO_U_Shr: Width -> MachOp
+| MO_S_Shr: Width -> MachOp
+(* Conversions.  Some of these will be NOPs.
+   Floating-point conversions use the signed variant. *)
+| MO_SF_Conv: Width -> Width -> MachOp
+| MO_FS_Conv: Width -> Width -> MachOp
+| MO_SS_Conv: Width -> Width -> MachOp
+| MO_UU_Conv: Width -> Width -> MachOp
+| MO_XX_Conv: Width -> Width -> MachOp
+| MO_FF_Conv: Width -> Width -> MachOp
+(* Vector element insertion and extraction operations *)
+| MO_V_Insert: Length -> Width -> MachOp
+| MO_V_Extract: Length -> Width -> MachOp
+(* Integer vector operations *)
+| MO_V_Add: Length -> Width -> MachOp
+| MO_V_Sub: Length -> Width -> MachOp
+| MO_V_Mul: Length -> Width -> MachOp
+(* Signed vector multiply/divide *)
+| MO_VS_Quot: Length -> Width -> MachOp
+| MO_VS_Rem: Length -> Width -> MachOp
+| MO_VS_Neg: Length -> Width -> MachOp
+(* Unsigned vector multiply/divide *)
+| MO_VU_Quot: Length -> Width -> MachOp
+| MO_VU_Rem: Length -> Width -> MachOp
+(* Floting point vector element insertion and extraction operations *)
+| MO_VF_Insert: Length -> Width -> MachOp
+| MO_VF_Extract: Length -> Width -> MachOp
+(* Floating point vector operations *)
+| MO_VF_Add: Length -> Width -> MachOp
+| MO_VF_Sub: Length -> Width -> MachOp
+| MO_VF_Neg: Length -> Width -> MachOp
+| MO_VF_Mul: Length -> Width -> MachOp
+| MO_VF_Quot: Length -> Width -> MachOp
+(* Alignment check (for -falignment-sanitisation) *)
+| MO_AlignmentCheck: Int -> Width -> MachOp
 .
 
+(*
 Definition MachOp_eq_dec (m1 m2 : MachOp) : {m1 = m2} + {m1 <> m2}.
   decide equality; try (apply Width_eq).
 Defined.
-
+*)
 Definition comparisonResultRep := bWord. (* but Haskellers are not sure about it :-) *)
                                     
 Definition machOpResultType (mo:MachOp) (tys:list CmmType) : CmmType :=
   match mo with
-  | MO_Add r => cmmBits r (* should be type of first argument
-                             if we are interested in GC-ptr-hood of values *)
-  | MO_Sub r => cmmBits r (* same as above *)
-  | MO_Mul r => cmmBits r
-  | MO_Eq _ => comparisonResultRep
-  | MO_Ne _ => comparisonResultRep
+  | MO_Add r => match tys with
+                | nil => cmmBits r (* should never happen*)
+                | cons h _ => h
+                end
+  | MO_Sub r => match tys with
+                | nil => cmmBits r (* should never happen*)
+                | cons h _ => h
+                end
+  | MO_Mul r
+  | MO_S_MulMayOflo r
+  | MO_S_Quot r
+  | MO_S_Rem  r
+  | MO_S_Neg  r
+  | MO_U_MulMayOflo r
+  | MO_U_Quot r
+  | MO_U_Rem  r => cmmBits r
+  | MO_Eq _
+  | MO_Ne _
+  | MO_S_Ge _
+  | MO_S_Le _
+  | MO_S_Gt _
+  | MO_S_Lt _
+  | MO_U_Ge _
+  | MO_U_Le _
+  | MO_U_Gt _
+  | MO_U_Lt _ => comparisonResultRep
+  | MO_F_Add r
+  | MO_F_Sub r
+  | MO_F_Mul r
+  | MO_F_Quot r
+  | MO_F_Neg r => cmmFloat r
+  | MO_F_Eq  _
+  | MO_F_Ne  _
+  | MO_F_Ge  _
+  | MO_F_Le  _
+  | MO_F_Gt  _
+  | MO_F_Lt  _  => comparisonResultRep
+  | MO_And r
+  | MO_Or r
+  | MO_Xor r => match tys with
+                | nil => cmmBits r (* should never happen*)
+                | cons h _ => h
+                end
+  | MO_Not   r
+  | MO_Shl   r
+  | MO_U_Shr r
+  | MO_S_Shr r => cmmBits r
+  | MO_SS_Conv _ to
+  | MO_UU_Conv _ to
+  | MO_XX_Conv _ to
+  | MO_FS_Conv _ to
+  | MO_SF_Conv _ to
+  | MO_FF_Conv _ to => cmmFloat to
+  | MO_V_Insert  l w => cmmVec l (cmmBits w)
+  | MO_V_Extract _ w => cmmBits w
+  | MO_V_Add l w
+  | MO_V_Sub l w
+  | MO_V_Mul l w
+  | MO_VS_Quot l w
+  | MO_VS_Rem  l w
+  | MO_VS_Neg  l w
+  | MO_VU_Quot l w
+  | MO_VU_Rem  l w => cmmVec l (cmmBits w)
+  | MO_VF_Insert  l w => cmmVec l (cmmFloat w)
+  | MO_VF_Extract _ w => cmmFloat w
+  | MO_VF_Add  l w
+  | MO_VF_Sub  l w
+  | MO_VF_Mul  l w
+  | MO_VF_Quot l w
+  | MO_VF_Neg  l w => cmmVec l (cmmFloat w)
+  | MO_AlignmentCheck _ _ => match tys with
+                             | nil => cmmBits r (* should never happen*)
+                             | cons h _ => h
+                             end 
   end.
 
 Inductive AtomicMachOp :=
--- ghc/CmmType.v
+++ ghc/CmmType.v
@@ -19,7 +19,7 @@ Definition CmmCat_eq (c1 c2 : CmmCat) : {c1=c2} + {c1<> c2}.
 Defined.
 
 Inductive Width :=
-  W8 | W16 | W32 | W64 | W128 | W256 | W512.
+  W8 | W16 | W32 | W64 | W80 | W128 | W256 | W512.
 
 Definition Width_eq (w1 w2 : Width) : {w1=w2} + {w1<>w2}.
   decide equality.
@@ -43,3 +43,20 @@ Definition cmmFloat : Width -> CmmType :=
 
 Definition b64 : CmmType := cmmBits W64.
 Definition bWord : CmmType := cmmBits W64. (* Need DynFlags here *) 
+
+Definition widthFromBytes (n:Int) : Width :=
+  match n with
+  | one => W8
+  | two => W16
+  | four => W32
+  | eight => W64
+  | sixteen => W128
+  | thirtytwo => W256
+  | sixtyfour => W512
+  | _ => W80
+  end.
+
+Definition cmmVec (n:Int) (t:CmmType) : CmmType :=
+  match t with
+  | CT_CmmType cat w => CT_CmmType (VecCat n cat) (widthFromBytes (n*widthInBytes w))
+  end.
