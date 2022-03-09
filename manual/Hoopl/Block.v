b58744ca548b6ce44ea8d0c3380ed19d29bf0e51
Peter Trommler <ptrommler@acm.org>
2022-03-09 10:02:37 +0100

    Translate some Hoopl.Block handling

--- manual/Hoopl/Block.v
+++ manual/Hoopl/Block.v
@@ -1,15 +1,20 @@
-Axiom C : Type.
-Axiom O : Type.
+
+Inductive oc : Set :=
+| O : oc
+| C : oc.
+
 
 Section Maybes.
   Parameter t : Type.
 
-Inductive MaybeO : Type -> Type -> Type :=
+Inductive MaybeO : oc -> Type -> Type :=
   JustO    : t -> MaybeO O t
 | NothingO :      MaybeO C t.
 End Maybes.
 
-Inductive Block (n:Type-> Type-> Type) : Type -> Type -> Type :=
+Print MaybeO.
+
+Inductive Block (n: oc -> oc -> Type) : oc -> oc -> Type :=
   BlockCO : n C O -> Block n O O          -> Block n C O
 | BlockCC : n C O -> Block n O O -> n O C -> Block n C C
 | BlockOC :          Block n O O -> n O C -> Block n O C
@@ -18,3 +23,27 @@ Inductive Block (n:Type-> Type-> Type) : Type -> Type -> Type :=
 | BCat    : Block n O O -> Block n O O -> Block n O O
 | BSnoc   : Block n O O -> n O O       -> Block n O O
 | BCons   : n O O       -> Block n O O -> Block n O O.
+
+Definition blockSplit {n:oc -> oc -> Type} (blk : Block n C C)
+  : n C O * Block n O O * n O C :=
+  match blk in Block _ e x return (match e, x with
+                                   | C, C => n C%type O * Block n O O *n O C%type
+                                   | _, _ => unit
+                                   end) with
+  | BlockCC _ f b t => (f, b, t)
+  | _ => tt
+  end.
+
+Fixpoint go {n : oc -> oc -> Type} (blk : Block n O O) (acc : list (n O O))
+  : list (n O O) :=
+  match blk in Block _ e x return (match e, x with
+                                   | O, O => list (n O O)
+                                   | _, _ => unit
+                                   end) with
+  | BNil _       => acc
+  | BMiddle _ n  => n :: acc
+  | BCat _ b1 b2 => go b1 (go b2 acc)
+  | BSnoc _ b1 n => go b1 (n :: acc)
+  | BCons _ n b1 => n :: go b1 acc
+  | _            => tt
+  end.
--- manual/Hoopl/Graph.v
+++ manual/Hoopl/Graph.v
@@ -17,13 +17,13 @@ Require Hoopl.Label.
 
 (* Converted type declarations: *)
 
-Definition Body' block (n : Type -> Type -> Type) :=
+Definition Body' block (n : Hoopl.Block.oc -> Hoopl.Block.oc -> Type) :=
   (Hoopl.Label.LabelMap (block n Hoopl.Block.C Hoopl.Block.C))%type.
 
 Section Graphs.
-  Parameter e x : Type.
+  Parameter e x : Hoopl.Block.oc.
 
-  Inductive Graph' block n : Type -> Type -> Type
+  Inductive Graph' block n : Hoopl.Block.oc -> Hoopl.Block.oc -> Type
     := | GNil : Graph' block n Hoopl.Block.O Hoopl.Block.O
   |  GUnit
     : block n Hoopl.Block.O Hoopl.Block.O ->
@@ -39,13 +39,14 @@ Definition Graph :=
 
 Definition Body n :=
   (Hoopl.Label.LabelMap (Hoopl.Block.Block n Hoopl.Block.C Hoopl.Block.C))%type.
+End Graphs.
 
 Arguments GNil {_} {_}.
 
-(* FIXME: Check what is neede here: Arguments GUnit {_} _ {_}. *)
+(* FIXME: Check what is needed here: Arguments GUnit {_} _ {_}. *)
 
 (* FIXME: Arguments GMany {_} {_} {_} {_}. *)
-End Graphs.
+
 (* Converted value declarations: *)
 
 (* Skipping all instances of class `Hoopl.Graph.NonLocal', including
